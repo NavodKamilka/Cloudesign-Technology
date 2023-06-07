8b7deb0b4e35d78dbeebea2f8c0af803a55e0672
Navod Kamilka <navod@tsdclanka.com>
2023-06-08 01:24:25 +0530

    add frontend files

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -10,3 +10,25 @@ function App() {
 }
 
 export default App;
+
+// import { Routes, Route ,BrowserRouter } from "react-router-dom";
+// import Post from './pages/Post.jsx';
+// import NewPost from './pages/NewPost.jsx';
+// import AppBar from './Components/AppBar.js';
+
+// function App() {
+//   return (
+//     <div>
+//       <BrowserRouter>
+//       <AppBar />
+//         <Routes>
+//           <Route path="/newPost" element={ <NewPost/> } />
+//           <Route path="/" element={ <Post/> } />
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
+
